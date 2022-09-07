3e51fb673f5cd268ac29cae5b2a93f316652d83c
sonwaboris <sonwaboris123@gmail.com>
2022-09-07 04:41:57 +0100

    cloudinary update

--- config/plugin.js
+++ config/plugin.js
@@ -8,4 +8,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "nanodevca",
+        api_key: "451287564133662",
+        api_secret: "P_7ezZcFxMeYN9RxagCFNtVzehs",
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
